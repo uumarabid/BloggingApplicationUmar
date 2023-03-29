84a53ad5b1717a46473018b049c821e5cb7b5f83
Umar1311829 <star_umar2008@hotmail.com>
2023-03-30 00:03:09 +0100

    feat!: Register couple of routes

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,11 +1,24 @@
 import "./App.css";
 // https://reactrouter.com/en/main/start/tutorial
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Write from "./pages/Write";
+import Single from "./pages/Single";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <div>This is Home</div>,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
 
