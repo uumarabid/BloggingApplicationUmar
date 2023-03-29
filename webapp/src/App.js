8e039b367e8ad60e55b29c1a7a882577be9159ab
Umar1311829 <star_umar2008@hotmail.com>
2023-03-30 00:21:37 +0100

    feat!: Refister routes with router outlet

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,17 +1,45 @@
 import "./App.css";
 // https://reactrouter.com/en/main/start/tutorial
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Write from "./pages/Write";
 import Single from "./pages/Single";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+// https://reactrouter.com/en/main/components/outlet
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>This is Home</div>,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/single",
+        element: <Single />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+    ],
   },
+  // routes without navbar and footer
   {
     path: "/register",
     element: <Register />,
