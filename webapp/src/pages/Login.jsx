2a6fe3873a2bafbeb23e3555bb3a1c8b01abd502
Umar1311829 <star_umar2008@hotmail.com>
2023-03-30 01:41:31 +0100

    feat!: Create register page & cleanup code

--- webapp/src/pages/Login.jsx
+++ webapp/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { Button, FormGroup, Grid, Paper, TextField } from "@mui/material";
+import { Button, FormControl, Grid, Paper, TextField } from "@mui/material";
 import { Container } from "@mui/system";
 import React from "react";
 import { Link } from "react-router-dom";
@@ -7,21 +7,18 @@ const Login = () => {
   return (
     <Container component="main" maxWidth="xs">
       <Paper variant="outlined" backgound="dark" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-        <FormGroup>
+        <FormControl>
           <legend>
             <h1>Welcome to login page</h1>
           </legend>
           <Grid container rowSpacing={2}>
-            <Grid item xs={4}>
-              {/* <label>Username</label> */}
-            </Grid>
             <Grid item xs={12}>
               <TextField
                 type="text"
-                id="user_name"
-                name="user_name"
+                id="username"
+                name="username"
                 placeholder="Enter your username"
-                label="username"
+                label="Username"
                 variant="outlined"
               />
             </Grid>
@@ -48,7 +45,7 @@ const Login = () => {
               </span>
             </Grid>
           </Grid>
-        </FormGroup>
+        </FormControl>
       </Paper>
     </Container>
   );
