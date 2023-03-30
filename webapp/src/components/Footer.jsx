6d09d02dcd4d7e1d58fa71c0f7281b3bc897a8af
Umar1311829 <star_umar2008@hotmail.com>
2023-03-30 03:00:20 +0100

    feat!: Add footer content

--- webapp/src/components/Footer.jsx
+++ webapp/src/components/Footer.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <GitHubIcon />
+      <LinkedInIcon />
+      <InstagramIcon />
+      <FacebookIcon />
+      <TwitterIcon />
+      <YouTubeIcon />
+    </footer>
+  );
 };
 
 export default Footer;
