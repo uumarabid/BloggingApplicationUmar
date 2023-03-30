4ee0ddd9157c820141dd85180be8c10e33222ca0
Umar1311829 <star_umar2008@hotmail.com>
2023-03-30 04:20:36 +0100

    feat!: Add some funtionality and content on home page

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -32,7 +32,7 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "/single",
+        path: "/post/:id",
         element: <Single />,
       },
       {
