ab283ba14b6d1cbffcc727c3bcd88e33bbba7d3e
Umar1311829 <star_umar2008@hotmail.com>
2023-04-04 03:58:06 +0100

    feat!: Created register api

--- webapi/controllers/auth.js
+++ webapi/controllers/auth.js
@@ -1,4 +1,30 @@
-export const register = (req, res) => {};
+import { db } from "../db.js";
+import bcrypt from "bcryptjs";
+
+export const register = (req, res) => {
+  //check existing user
+  const q = "SELECT * FROM users WHERE email = ? OR username = ?";
+
+  db.query(q[(req.body.email, req.body.name)], (err, data) => {
+    if (err) return res.json(err);
+    // if user exits
+    if (data.length) return res.send("User already exist");
+
+    // hash a password and create a user
+    // https://www.npmjs.com/package/bcryptjs
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
+    // insesrt user to database
+    const q = "INSERT INTO users (`username`, `email`, `password`) VALUES (?)";
+    const values = [req.body.username, req.body.email, hash];
+
+    db.query(q[values], (err, data) => {
+      if (err) return res.json(err);
+      return res.send("User has been created.");
+    });
+  });
+};
 
 export const login = (req, res) => {};
 
