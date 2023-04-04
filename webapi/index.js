b43c3a8fbbf0b36d5b311bc297aa786c77332371
Umar1311829 <star_umar2008@hotmail.com>
2023-04-04 03:19:09 +0100

    feat!: Basic routing struture and controllers

--- webapi/index.js
+++ webapi/index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import router from "./routes/posts.js";
+import authRoutes from "./routes/auth.js";
+import usersRoutes from "./routes/users.js";
+import postsRoutes from "./routes/posts.js";
 
 const app = express();
 const port = 3001;
@@ -8,6 +12,9 @@ app.get("/", (req, res) => {
 });
 
 app.use(express.json());
+app.use("/auth", authRoutes);
+app.use("/user", usersRoutes);
+app.use("/posts", postsRoutes);
 
 app.listen(port, () => {
   console.log(`Example app is listening on port ${port}`);
