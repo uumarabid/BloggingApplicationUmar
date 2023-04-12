0264640e7cea52c611c8026cd77cd215eb985896
Umar1311829 <star_umar2008@hotmail.com>
2023-04-13 00:45:44 +0100

    feat: working on login api

--- webapi/controllers/auth.js
+++ webapi/controllers/auth.js
@@ -27,6 +27,20 @@ export const register = (req, res) => {
   });
 };
 
-export const login = (req, res) => {};
+export const login = (req, res) => {
+  // check user existance
+  const q = "SELECT * FROM users  WHERE username = ?";
+
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) return res.json(err);
+    // another condition if no user in db
+    if (data.length === 0) return res.status(404).json("User not found.!");
+    // if no err and user exists check password
+    // comapre the plain text with hased passed using comapring function below
+    const isPasswordCorrect = bcrypt.compareSync(req.body.password, data[0].password);
+
+    if (!isPasswordCorrect) return res.status(400).json("Wrong username or password.!");
+  });
+};
 
 export const logout = (req, res) => {};
