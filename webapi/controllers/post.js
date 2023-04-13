68f3d018bec322d5f69e51bd87b587821537c0d8
Umar1311829 <star_umar2008@hotmail.com>
2023-04-13 03:39:11 +0100

    feat: Add route for all posts and working on post api's

--- webapi/controllers/post.js
+++ webapi/controllers/post.js
@@ -1,3 +1,19 @@
+export const getPosts = (req, res) => {
+  res.json("New post is added successfully.");
+};
+
+export const getPost = (req, res) => {
+  res.json("New post is added successfully.");
+};
+
 export const addPost = (req, res) => {
+  res.json("New post is added successfully.");
+};
+
+export const deletePost = (req, res) => {
+  res.json("New post is added successfully.");
+};
+
+export const updatePost = (req, res) => {
   res.json("New post is added successfully.");
 };
